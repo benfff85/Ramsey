97e1a70a19bcf3dbfbfd0072fb90f18c781a0dc3
Ben Ferenchak <ben.ferenchak@gmail.com>
2016-05-18 21:32:42 -0400

    Updated RamseyLogger to use SLF4J.

--- src/main/java/com/benajaminleephoto/ramsey/common/RamseyLogger.java
+++ src/main/java/com/benajaminleephoto/ramsey/common/RamseyLogger.java
@@ -1,12 +1,11 @@
 package com.benajaminleephoto.ramsey.common;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This Class will help facilitate logging to a file as well as tracking various values to gauge
@@ -17,12 +16,11 @@ import java.util.Date;
  */
 public class RamseyLogger {
 
-    private BufferedWriter bufferedLogWriter;
-    private String formattedLogDate;
     private CayleyGraph cayleyGraph;
     private Timer timer;
     private CumulativeStatistics stats;
     private CliqueCollectionSnapshot cliqueCollectionSnapshot;
+    private static final Logger logger = LoggerFactory.getLogger(RamseyLogger.class.getName());
 
 
     /**
@@ -33,60 +31,10 @@ public class RamseyLogger {
      * @param stats Statistics object which can be pulled for cumulative statistics of the run.
      */
     public RamseyLogger(CayleyGraph cayleyGraph, Timer timer, CumulativeStatistics stats) {
-        formattedLogDate = getDateTimeStamp();
         this.cayleyGraph = cayleyGraph;
         this.timer = timer;
         this.stats = stats;
         cliqueCollectionSnapshot = new CliqueCollectionSnapshot();
-    }
-
-
-    /**
-     * This will take an input String of content and write it to a log file. If the log file is not
-     * opened it will open it. All content will append to the end of the file if the file is already
-     * opened.
-     * 
-     * @param content The String content which will be written to the log file.
-     */
-    private void writeToLogFile(String content) {
-        if (bufferedLogWriter == null) {
-            openLogFile();
-        }
-
-        try {
-            bufferedLogWriter.append(content);
-            bufferedLogWriter.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    /**
-     * This is a public method to close the BufferedWritter used by this class.
-     */
-    public void closeLogFile() {
-        try {
-            bufferedLogWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    /**
-     * This is a local method used to open a new file if the public BufferedWriter is not already
-     * opened.
-     */
-    private void openLogFile() {
-        String fileName = Config.LOG_FILE_PATH + Config.LOG_FILE_MASK + formattedLogDate + ".log";
-        try {
-            File file = new File(fileName);
-            FileWriter fw = new FileWriter(file.getAbsoluteFile());
-            bufferedLogWriter = new BufferedWriter(fw);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 
@@ -100,11 +48,9 @@ public class RamseyLogger {
      * @param cayleyGraph The CayleyGraph object the process is being applied to (Counter Example)
      */
     public void processPositiveCase(CayleyGraph cayleyGraph) {
-        System.out.println(cayleyGraph.printCayleyGraphMathematica());
-        writeToLogFile("SOLUTION:\n" + cayleyGraph.printCayleyGraphMathematica());
+        logger.info("SOLUTION:\n" + cayleyGraph.printCayleyGraphMathematica());
         GraphFileWriter.writeSolutionFile();
         // cayleyGraph.emailCayleyGraph();
-        closeLogFile();
     }
 
 
@@ -114,16 +60,14 @@ public class RamseyLogger {
      */
     public void processCheckpoint() {
         if (stats.getMinCliqueCount() == cayleyGraph.getCliqueCollection().getCliqueCount()) {
-            System.out.println("Writing MAX File");
             GraphFileWriter.writeMaxFile();
             cliqueCollectionSnapshot.clearCliqueCollectionArray();
             cliqueCollectionSnapshot.populateSnapshot(cayleyGraph.getCliqueCollection());
             if (Config.LAUNCH_METHOD != LAUNCH_TYPE.OPEN_FROM_FILE || stats.getAnalyzedGraphCount() != 1) {
-                System.out.println("Writing CHK File");
                 GraphFileWriter.writeCheckpointFile();
             }
         } else {
-            System.out.println("Rolling Back");
+            logger.info("Rolling Back");
             cayleyGraph.rollback(Config.CHKPT_FILE_PATH + "Ramsey_MAX.chk", cliqueCollectionSnapshot);
             System.gc();
         }
@@ -133,7 +77,7 @@ public class RamseyLogger {
     public void logIteration() {
         // Write to log if appropriate
         if (stats.getAnalyzedGraphCount() % Config.LOG_INTERVAL == 0) {
-            writeToLog(printSummaryInfo());
+            logger.info(getSummaryInfo());
             timer.clearCumulativeDuration();
         }
     }
@@ -144,20 +88,9 @@ public class RamseyLogger {
      * 
      * @return String of various data elements to be logged.
      */
-    private String printSummaryInfo() {
-        String content = stats.getAnalyzedGraphCount() + "|" + cayleyGraph.getClique().getColor() + "|" + cayleyGraph.getClique().printClique() + "|" + cayleyGraph.printRedBlueCount() + "|" + cayleyGraph.printDistributionSummary("RED") + "|" + cayleyGraph.getCliqueCollection().getCliqueCount() + "|" + stats.getMinCliqueCount() + "|" + timer.printCumulativeDuration("MUTATE") + "|" + timer.printCumulativeDuration("LOGGER") + "|" + timer.printCumulativeDuration("CLIQUE") + "|" + timer.printCumulativeDuration("STATS") + "|" + cayleyGraph.printDistribution("RED") + "\n";
-
+    private String getSummaryInfo() {
+        String content = stats.getAnalyzedGraphCount() + "|" + cayleyGraph.printDistributionSummary("RED") + "|" + cayleyGraph.getCliqueCollection().getCliqueCount() + "|" + stats.getMinCliqueCount() + "|" + timer.printCumulativeDuration("MUTATE") + "|" + timer.printCumulativeDuration("LOGGER") + "|" + timer.printCumulativeDuration("CLIQUE") + "|" + timer.printCumulativeDuration("STATS") + "|" + cayleyGraph.printDistribution("RED");
         return content;
-    }
-
-
-    private void writeToLog(String content) {
-        if (Config.LOG_METHOD == LOG_TYPE.FILE || Config.LOG_METHOD == LOG_TYPE.BOTH) {
-            writeToLogFile(content);
-        }
-        if (Config.LOG_METHOD == LOG_TYPE.CONSOLE || Config.LOG_METHOD == LOG_TYPE.BOTH) {
-            System.out.print(content);
-        }
     }
 
 
